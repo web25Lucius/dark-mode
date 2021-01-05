d01c3939bcb93b5a2105fb984ba5220a7b7a7518
web25Lucius <web25Lucius@gmail.com>
2021-01-04 17:14:14 -0700

    page rendering, adding toggle to body

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,8 +1,52 @@
-import {useState} from 'react';
+import React, {useState} from 'react';
 
-export const useLocalStorage = ( key, initialValue ) => {
-const [mode, setMode] = useState (()=>{
-    const keyValue = window.localStorage.getItem(key);
-})
+// export const useLocalStorage = (key, initialValue) => {
+//     const [storedValue, setStoredValue] = useLocalStorage(key, initialValue);
+//     const 
+    
+// }
 
-}
+
+
+
+
+export const useLocalStorage = (key, initialValue) => {
+    const [storedValue, setStoredValue] = useState(() => {
+    const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    });
+    const setValue = value => {
+      setStoredValue(value);
+      window.localStorage.setItem(key, JSON.stringify(value));
+    };
+    return [storedValue, setValue];
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import {useState} from 'react';
+
+// export const useLocalStorage = ( key, initialValue ) => {
+// const [mode, setMode] = useState (()=>{
+//     const keyValue = window.localStorage.getItem(key);
+// })
+
+// }
